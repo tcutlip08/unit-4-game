8b0f663dda69e7bff2ba497a1f86e111ed059d7f
unknown <tcutlip08@gmail.com>
2019-06-21 20:18:56 -0400

    made all the (pick enemy) buttons turn red after you pick a character. display hp of each character in button. made my 4 sepparate object into 1 (stats) all fully functional

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,85 +10,123 @@ $(document).ready(function () {
     // else if the enemy's hp is <= 0 make the enemy's picture/buttons dissapear and allow them to select another enemy
 
     var characterHP = 0;
-    var characterAtt = 5;
+    var characterAtt = 8;
+    var characterIncAtt = characterAtt;
     var enemyHP = 0;
     var enemyCounter = 0;
-    var obi = { hp: 120, counter: 10 }
-    var luke = { hp: 100, counter: 5 }
-    var maul = { hp: 150, counter: 15 }
-    var vader = { hp: 180, counter: 20 }
+    var stats = {
+        obi: { hp: 120, counter: 15 },
+        luke: { hp: 100, counter: 20},
+        maul: { hp: 150, counter: 12 },
+        vader: { hp: 180, counter: 10 }
+    };
+
+    displayStartingStats(stats);
 
     $("#button-Obi").on("click", function () {
-        setStats(obi);
+        setStats(stats.obi);
         // var img = document.getElementById("button-Obi").style.display = "none";
     });
 
     $("#button-Luke").on("click", function () {
-        setStats(luke);
+        setStats(stats.luke);
     });
 
     $("#button-Maul").on("click", function () {
-        setStats(maul);
+        setStats(stats.maul);
     });
 
     $("#button-Vader").on("click", function () {
-        setStats(vader);
+        setStats(stats.vader);
     });
 
     $("#button-attack").on("click", function () {
-        if (characterHP !== 0 && enemyHP !== 0) {
+        if ((characterHP !== 0) && (enemyHP !== 0)) {
             characterHP = characterHP - enemyCounter;
-            enemyHP = enemyHP - characterAtt;
+            enemyHP = enemyHP - characterIncAtt;
             console.log("Your HP: " + characterHP);
-            console.log("Your Att: " + characterAtt);
+            console.log("Your Att: " + characterIncAtt);
             console.log("Enemy's HP: " + enemyHP);
         }
-        characterAtt = characterAtt + 5;
+
+        if (characterHP <= 0) {
+            console.log("You Lose");
+        } else if (enemyHP <= 0) {
+            console.log("You Win")
+            enemyHP = 0;
+        }
+
+        if (enemyHP === 0) {
+            $("#chosenEnemy").remove();
+        }
+        characterIncAtt = characterIncAtt + characterAtt;
     });
 
     function setStats(name) {
         if (characterHP === 0) {
             characterHP = name.hp;
 
-            if (name === obi) {
+            if (name === stats.obi) {
                 document.getElementById("button-Obi").style.display = "none";
                 var r = $('<button id="chosenCharacter" class="btn btn-success"><img width="200" height="150" src="assets/images/ObiWan.jpg"></button>');
                 $("#displayCharacter").append(r);
-            } else if (name === luke) {
+                setButtonRed();
+            } else if (name === stats.luke) {
                 document.getElementById("button-Luke").style.display = "none";
                 var r = $('<button id="chosenCharacter" class="btn btn-success"><img width="200" height="150" src="assets/images/LukeSkywalker.jpg"></button>');
                 $("#displayCharacter").append(r);
-            } else if (name === maul) {
+                setButtonRed();
+            } else if (name === stats.maul) {
                 document.getElementById("button-Maul").style.display = "none";
                 var r = $('<button id="chosenCharacter" class="btn btn-success"><img width="200" height="150" src="assets/images/DarthMaul.jpg"></button>');
                 $("#displayCharacter").append(r);
-            } else if (name === vader) {
+                setButtonRed();
+            } else if (name === stats.vader) {
                 document.getElementById("button-Vader").style.display = "none";
                 var r = $('<button id="chosenCharacter" class="btn btn-success"><img width="200" height="150" src="assets/images/DarthVader.jpg"></button>');
                 $("#displayCharacter").append(r);
+                setButtonRed();
             }
         }
-        else if (characterHP !== 0) {
+        else if (characterHP !== 0 && enemyHP === 0) {
             enemyHP = name.hp;
             enemyCounter = name.counter;
 
-            if (name === obi) {
+            if (name === stats.obi) {
                 document.getElementById("button-Obi").style.display = "none";
-                var r = $('<button id="chosenCharacter" class="btn btn-danger"><img width="200" height="150" src="assets/images/ObiWan.jpg"></button>');
+                var r = $('<button id="chosenEnemy" class="btn btn-danger"><img width="200" height="150" src="assets/images/ObiWan.jpg"></button>');
                 $("#displayEnemy").append(r);
-            } else if (name === luke) {
+            } else if (name === stats.luke) {
                 document.getElementById("button-Luke").style.display = "none";
-                var r = $('<button id="chosenCharacter" class="btn btn-danger"><img width="200" height="150" src="assets/images/LukeSkywalker.jpg"></button>');
+                var r = $('<button id="chosenEnemy" class="btn btn-danger"><img width="200" height="150" src="assets/images/LukeSkywalker.jpg"></button>');
                 $("#displayEnemy").append(r);
-            } else if (name === maul) {
+            } else if (name === stats.maul) {
                 document.getElementById("button-Maul").style.display = "none";
-                var r = $('<button id="chosenCharacter" class="btn btn-danger"><img width="200" height="150" src="assets/images/DarthMaul.jpg"></button>');
+                var r = $('<button id="chosenEnemy" class="btn btn-danger"><img width="200" height="150" src="assets/images/DarthMaul.jpg"></button>');
                 $("#displayEnemy").append(r);
-            } else if (name === vader) {
+            } else if (name === stats.vader) {
                 document.getElementById("button-Vader").style.display = "none";
-                var r = $('<button id="chosenCharacter" class="btn btn-danger"><img width="200" height="150" src="assets/images/DarthVader.jpg"></button>');
+                var r = $('<button id="chosenEnemy" class="btn btn-danger"><img width="200" height="150" src="assets/images/DarthVader.jpg"></button>');
                 $("#displayEnemy").append(r);
             }
         }
+    }
+
+    function setButtonRed() {
+        $("#button-Obi").removeClass("btn btn-success").addClass("btn btn-danger");
+        $("#button-Luke").removeClass("btn btn-success").addClass("btn btn-danger");
+        $("#button-Maul").removeClass("btn btn-success").addClass("btn btn-danger");
+        $("#button-Vader").removeClass("btn btn-success").addClass("btn btn-danger");
+    }
+
+    function displayStartingStats(stats) {
+        var obi = stats.obi.hp;
+        $("#button-Obi").append("<br>HP: " + obi);
+        var luke = stats.luke.hp;
+        $("#button-Luke").append("<br>HP: " + luke);
+        var maul = stats.maul.hp;
+        $("#button-Maul").append("<br>HP: " + maul);
+        var vader = stats.vader.hp;
+        $("#button-Vader").append("<br>HP: " + vader);
     }
 });
